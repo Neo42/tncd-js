7b014fa8f3f9445fd94ca079cf4f2ae386b62d3f
墨石 <582307236@qq.com>
2019-09-24 16:42:22 +0800

    Update lesson 2

---
+++ 2-code-structure/main.js
@@ -0,0 +1,12 @@
+// This is a comment.
+/* Also a comment */
+// Hot key comment
+
+// alert('What?')
+// alert('No')
+
+// alert("There will be an error")
+// [1, 2].forEach(alert)
+
+alert('Use');
+alert('Simicolon');
