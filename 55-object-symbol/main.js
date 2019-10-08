681ca6748e92431ff281c43aec608337378ad8ec
墨石 <582307236@qq.com>
2019-10-08 11:30:12 +0800

    Added symbol chapter.

--- 55-object-symbol/main.js
+++ 55-object-symbol/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const people = {};
+const emptyObj = {};
+people.james = 'Lebron James';
+people[Symbol('bob')] = { name: 'Bob Smith' };
+people[Symbol('bob')] = { name: 'Bob Jones' };
+people;
+
+// assign, Reflect.ownKeys, []access
+Object.assign(emptyObj, people);
+console.log(emptyObj);
+console.log(Reflect.ownKeys(people));
+
+// for in loop, keys, getOwnPropertyNames
+for (let person in people) {
+  console.log(person);
+}
+console.log(Object.keys(people));
+console.log(Object.getOwnPropertyNames(people));
