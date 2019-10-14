8dc1966265fc0749ba853e9f2bc0a2d5f8787d95
Jiang Hao <582307236@qq.com>
2019-10-14 23:56:23 +0800

    UPdated.

--- 69-array-methods-searching/main.js
+++ 69-array-methods-searching/main.js
@@ -2,7 +2,7 @@
 
 let myArray = ['a', 'b', 'c'];
 
-// arr.indexOf(element, start):从起始位置 start 开始从左到右用 === 搜索元素 element，不转换元素类型
+// arr.indexOf(element, start):从起始位置 start 开始从左到右用三联等号 === 搜索元素 element，不转换元素类型
 console.log(myArray.indexOf('b', 0)); // 1
 console.log(myArray.lastIndexOf('b')); // 1，lastIndexOf 从右到左搜索
 console.log(myArray.includes('b')); // true，includes 存在性检查
@@ -17,10 +17,6 @@ console.log(myArray.lastIndexOf(NaN)); // -1，同理
 myArray = [{ id: 1 }, { id: 2 }, { id: 3 }];
 myArray.find(ele => console.log(ele)); // 输出每个元素对象
 console.log(myArray.find(ele => ele.id === 2)); // { id:2 } 返回并输出对象属性 id 为 2 的 元素
-console.log(
-  myArray.find((ele, i, array) => {
-    // 也可以不使用参数，这里直接跳过了 element
-    console.log(i); // 0, 1, 2
-    return i > array.length / 2 ? true : false;
-  })
-); // { id: 3 }，返回 index > 1.5 的元素 ele
+
+// 也可以不使用参数，这里直接跳过了 element
+console.log(myArray.find((ele, i, array) => i > array.length / 2)); // { id: 3 }，返回 index > 1.5 的所有元素
