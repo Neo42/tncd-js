27b94f972aafbb5a0ef0a16a1f7ed6ad1fa83673
Jiang Hao <582307236@qq.com>
2019-10-16 00:40:00 +0800

    UPdated.

--- 65-string-advanced/main.js
+++ 65-string-advanced/main.js
@@ -3,9 +3,9 @@
 // unicode编码位置、码点、代码点、码位：字符在 unicode 编码中的编号，可以用不同进制表示 e.g.‘&#128514;’
 // UTF-16：UTF-16是 unicode 字符的一种表示格式，用一个或两个（二进制下）长度为 16 位的整数序列（即代码单元或码元）表示 unicode 编码位置，可以使用不同进制表示。 e.g. 16进制（hex）：0xD83D 0xDE02。
 // 高位编码单元（unicode 编码位置高于 65535）使用一对（低位编码 lower valued）代理伪字符（"surrogate" pseudo-characters）来表示，从而构成一个真正的字符。
-// 字符 (😂) <--> utf-16 十六进制编码单元（代理对）：d83d de02 <--> js转义符（跟 UTF-16 编码单元对应）："\ud83d\ude02" <--> 整个字符的 unicode 十进制编码位置(码位、代码点)：&#128514;
+
 // 代理对的两段 utf-16 编码单元
-console.log('\ud83d\ude02');
+console.log('\ud83d\ude02'); // 😂
 
 // 高位编码单元的长度为 2
 console.log('😂'.length);
@@ -34,6 +34,7 @@ for (let index = 0; index < '😂'.length; index++) {
 console.log('S\u0307'); // Ṡ
 console.log('S\u0307\u0323'); // Ṩ
 
+// 正规化得到完整字符，转换为 unicode 码点，再转换为 UTF-16 编码单元
 console.log(
   'S\u0307\u0323'
     .normalize()
--- 75-array-methods-reduce/main.js
+++ 75-array-methods-reduce/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
-// 为什么： 在数量，程度或大小上变小或变小。
+// reduce：在数量，程度或大小上变小或变小
 // reduce缩小, fold折叠, accumulate积累, aggregate合计, compress压缩, inject注入 (https://en.wikipedia.org/wiki/Fold_(higher-order_function)
 // 将列表“折叠”或“缩小”（或“折叠”）成单个值 (https://wiki.tcl-lang.org/page/Fold+in+functional+programming)
 
+let myArray = ['a', 'b', 3];
+console.log(myArray.reduce(() => {}));
