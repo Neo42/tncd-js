36c8461241bc39462705aaa80f15afe1b3b0b707
墨石 <582307236@qq.com>
2019-10-16 10:48:53 +0800

    UPdated.

--- 75-array-methods-reduce/main.js
+++ 75-array-methods-reduce/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // reduce：在数量，程度或大小上变小或变小
-// reduce缩小, fold折叠, accumulate积累, aggregate合计, compress压缩, inject注入 (https://en.wikipedia.org/wiki/Fold_(higher-order_function)
+// reduce缩小, fold折叠, accumulate积累, aggregate合计, compress压缩, inject注入 (https://en.wikipedia.org/wiki/Fold_(higher-order_function))
 // 将列表“折叠”或“缩小”（或“折叠”）成单个值 (https://wiki.tcl-lang.org/page/Fold+in+functional+programming)
 
 let myArray = ['a', 'b', 3];
