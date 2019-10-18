f005062f39e964e50a3e689c82510989142c2d4d
墨石 <582307236@qq.com>
2019-10-18 18:30:06 +0800

    UPdate

--- 41-object-fundamental/main.js
+++ 41-object-fundamental/main.js
@@ -5,13 +5,11 @@ let user = new Object(); // “构造函数” 的语法
 user.name = '墨石';
 delete user.name;
 
-// set
 // user.likes birds = true
-user['likes birds'] = true;
-
+user['likes the Shiba Inu'] = true;
+console.log(Object.keys(user)); // [ 'likes the Shiba Inu' ]
 console.log(user['likes birds']);
 delete user['likes birds'];
 
 // 保留字可以当属性名
 user.let = 'me';
-user;
--- 83-map/main.js
+++ 83-map/main.js
@@ -8,12 +8,22 @@ console.log(typeof aMap); // object, map 仍然是对象类型
 
 // map.set()：返回新 map，可以链式操作
 let myMap = new Map();
-myMap
-  .set('a string', "和键'a string'关联的值")
-  .set({}, '和键keyObj关联的值')
-  .set(function() {}, '和键keyFunc关联的值');
+let keyStr = 'a string',
+  keyFunc = function() {},
+  keyObj = {},
+  keyArr = [],
+  keyNull = null;
 
-// 读取值, 按 === 查找键
-console.log(myMap.get('a string')); // "和键'a string'关联的值"，因为'a string' === 'a string'
-console.log(myMap.get({})); // undefined, 因为{} !== {}
-console.log(myMap.get(function() {})); // undefined, 因为function() {} !== function() {}
+myMap
+  .set(keyStr, '和键keyStr关联的值')
+  .set(keyObj, '和键keyObj关联的值')
+  .set(keyFunc, '和键keyFunc关联的值')
+  .set(keyArr, '和键keyArr关联的值')
+  .set(keyNull, '和键keyNull关联的值');
+
+// 读取值, 按 === 查找键且认定 null === null
+console.log(myMap.get('a string')); // "和键keyStr关联的值"，因为 'a string' === 'a string'
+console.log(myMap.get({})); // undefined, 因为 {} !== {}
+console.log(myMap.get(function() {})); // undefined, 因为 function() {} !== function() {}
+console.log(myMap.get([])); // undefined, 因为 [] !== []
+console.log(myMap.get(null)); // 和键keyNull关联的值, 因为此处认为 null === null
