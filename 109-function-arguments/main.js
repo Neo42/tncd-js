dd24767f5905ce3213a41a4aa89e7053be7da27e
墨石 <582307236@qq.com>
2019-10-29 15:38:35 +0800

    Updated.

--- 109-function-arguments/main.js
+++ 109-function-arguments/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function func(a, b, c) {
+  console.log(typeof arguments, arguments);
+  console.log(Object.entries(arguments)); // [ [ '0', 'a' ], [ '1', 'b' ], [ '2', 'c' ] ]
+  console.log(arguments.length); // 3
+
+  console.log(arguments[Symbol.iterator]); // ƒ values() { [native code] }
+  // console.log(arguments.callee); // Uncaught TypeError: 'caller', 'callee', and 'arguments' properties may not be accessed on strict mode functions or the arguments objects for calls to them
+
+  for (const i of arguments) {
+    console.log(i); // a,b,c
+  }
+}
+
+func('a', 'b', 'c');
