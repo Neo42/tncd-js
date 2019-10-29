8859bb70e18d41eaf4969a547027acd54a5d58c4
墨石 <582307236@qq.com>
2019-10-29 14:59:31 +0800

    Updated.

--- 108-function-with-spread-rest/main.js
+++ 108-function-with-spread-rest/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+// spread：展开各个元素
+let spreadArray = [21, 23, 45];
+console.log(...spreadArray); // 21 23 45 ​​​​​
+
+// rest：收集剩余元素，构成数组
+// [...rest] = arguments.slice(2)
+function log1(a, b, ...rest) {
+  // 收集除第一、二个以外的剩余的参数，构成数组 rest
+  console.log(a, b, rest); // 输出 rest 数组
+}
+
+// [a, b, ...rest] = [...spreadArray]
+log1(...spreadArray); // 21 23 [ 45 ]
+
+// 合用
+function log2(a, b, ...rest) {
+  console.log(a, b, ...rest); // 展开 rest 的每个元素再输出
+}
+log2(...spreadArray); // 21 23 45 ​​​​​
+
+let arr = [1, 2, 3];
+// 嵌套解构
+function log3([...rest]) {
+  // 实参必须是数组
+  console.log(rest);
+}
+
+// [[...rest]] = [arr]
+log3(arr); // [ 1, 2, 3 ]
--- 93-rest-spread/main.js
+++ 93-rest-spread/main.js
@@ -3,17 +3,20 @@
 let smallArray = [5, 6, 7];
 let bigArray = [1, 2, 3, 4, ...smallArray, 8, 9, 0];
 
-console.log(bigArray);
+console.log(bigArray); // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ]
 
-// let [...anotherArray, a, b] = bigArray; // Rest element must be last element 解构赋值，rest元素
-// let [a, b, ...anotherArray] = bigArray;
-// console.log(anotherArray);
+let [a, b, ...anotherArray] = bigArray;
+console.log(anotherArray); // [ 3, 4, 5, 6, 7, 8, 9, 0 ]
 
 let [...rest] = [2, 1]; // rest
 console.log(rest);
 
-let [b] = [...[1, 2]]; // spread
-console.log(b);
+// Rest element must be last element,剩余元素必须排在最后
+// let [...rest, a, b] = [1, 2, 3];
+
+// spread
+[a] = [...[1, 2]];
+console.log(a);
 
 let arr = [3, 5, 1];
 console.log(Math.max(...arr));
