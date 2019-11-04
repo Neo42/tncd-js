899072254d2adf953c988ab9c4731aafd59dcd3e
墨石 <582307236@qq.com>
2019-11-04 15:41:20 +0800

    🚀🚀🚀 You're A badass!!! 🚀🚀🚀

--- 112-scope/main.js
+++ 112-scope/main.js
@@ -8,7 +8,7 @@ function f() {
 // 作用域
 // MDN定义：当前的执行上下文（一个变量或函数所在的执行上下文）https://developer.mozilla.org/en-US/docs/Glossary/Scope
 // 跟一般意义上的执行上下文所指范围不同, 两个概念的使用场景也不同
-// 别称：词法环境、词法作用域——JavaScript引擎根据一个变量或函数在整个脚本中的词法位置判断它所在的作用域，而不是动态判断（动态作用域）
+// 别称：词法环境、词法作用域—— JS引擎根据声明变量的位置来确定该变量可被访问的位置
 // (规定了)变量和函数的可见性——一个标识符（变量名称、函数名称）能准确引用到某个变量的位置、范围
 
 // 全局作用域
@@ -53,3 +53,52 @@ function returnFunc() {
 
 let string = 'global';
 console.log(returnFunc()); // local, 沿作用域链从内向外查找：returnedFunc => returnFunc => Global
+
+var result = [];
+
+for (var i = 0; i < 5; i++) {
+  result[i] = function() {
+    console.log(i);
+  };
+}
+
+console.log(i);
+
+result[0](); // 5, expected 0
+result[1](); // 5, expected 1
+result[2](); // 5, expected 2
+result[3](); // 5, expected 3
+result[4](); // 5, expected 4
+
+// 全局创建阶段：全局执行上下文 this、null、global对象，变量：result、i，函数
+// 全局执行阶段：先进行循环，定义新函数
+// result = [
+//   function(){console.log(i);},
+//   function(){console.log(i);},
+//   function(){console.log(i);},
+//   function(){console.log(i);},
+//   function(){console.log(i);}
+// ]
+// 之后发现函数调用，再取用变量 i（全局变量 i = 5），函数执行
+
+function showHelp(help) {
+  document.getElementById('help').innerHTML = help;
+}
+
+function setupHelp() {
+  var helpText = [
+    { id: 'email', help: 'Your e-mail address' },
+    { id: 'name', help: 'Your full name' },
+    { id: 'age', help: 'Your age (you must be over 16)' }
+  ];
+
+  for (var i = 0; i < helpText.length; i++) {
+    var item = helpText[i]; // 全局 i
+    document.getElementById(item.id).onfocus = function() {
+      console.log(i);
+      showHelp(item.help);
+    };
+  }
+}
+
+setupHelp();
--- 113-closure/main.js
+++ 113-closure/main.js
@@ -1,14 +1,18 @@
 'use strict';
 
 let weapon = 'sword';
+// 函数声明
 function getWeapon() {
   console.log(`Got weapon: ${weapon}`);
   let innerWeapon = 'gun';
 }
 
+// 函数实例创建
 getWeapon(); // Got weapon: sword
-console.log(innerWeapon); // 未定义
+// console.log(innerWeapon); // 未定义
 
-// 所有函数（的实例）中都有一个隐式引用（属性） [[environment]] 允许函数可以从内部访问到外层作用域（词法环境）中的变量和函数
+// 闭包：能够记录和访问它的外层作用域中变量和函数的函数
+// 所有函数（的实例）中都有一个隐式引用（属性） [[environment]]
 // 但外层作用域没办法访问到函数作用域中的变量和函数
 // 闭包是一个相对概念：函数相对于它的外层作用域（词法环境）来说是一个闭包
+// 闭包涉及两个组成部分：函数和它的外层作用域共同组成
