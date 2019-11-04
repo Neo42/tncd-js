6882702b89d75c1172996948c6b90b53b6dd22a8
墨石 <582307236@qq.com>
2019-11-04 10:58:14 +0800

    Committed with Git-Upload

--- 113-closure/main.js
+++ 113-closure/main.js
@@ -1,11 +1,14 @@
 'use strict';
 
-function closureFunc() {
-  let localVar = 'localVar';
-  console.log(localVar);
+let weapon = 'sword';
+function getWeapon() {
+  console.log(`Got weapon: ${weapon}`);
+  let innerWeapon = 'gun';
 }
 
-closureFunc(); // localVar
-// console.log(localVar); // 未定义
+getWeapon(); // Got weapon: sword
+console.log(innerWeapon); // 未定义
 
-// 所有函数执行环境中都有一个隐式属性 [[environment]]
+// 所有函数（的实例）中都有一个隐式引用（属性） [[environment]] 允许函数可以从内部访问到外层作用域（词法环境）中的变量和函数
+// 但外层作用域没办法访问到函数作用域中的变量和函数
+// 闭包是一个相对概念：函数相对于它的外层作用域（词法环境）来说是一个闭包
