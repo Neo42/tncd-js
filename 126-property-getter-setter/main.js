3c5f3357441a587cacf2b6e383888869467bd4de
墨石 <582307236@qq.com>
2019-11-11 18:34:56 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 126-property-getter-setter/main.js
+++ 126-property-getter-setter/main.js
@@ -2,15 +2,47 @@
 
 let man = {
   firstName: 'Creator',
-  surname: 'of JavaScript',
-  get fullName() {
+  surname: 'of JavaScript'
+  // get fullName() {
+  //   return this.firstName + ' ' + this.surname;
+  // },
+  // set fullName(value) {
+  //   [this.firstName, this.surname] = value.split(' ');
+  // }
+};
+
+Object.defineProperty(man, 'fullName', {
+  get() {
     return this.firstName + ' ' + this.surname;
   },
-  set fullName(value) {
+  set(value) {
     [this.firstName, this.surname] = value.split(' ');
   }
-};
+});
 
 console.log(man.fullName); // Creator of JavaScript
 man.fullName = 'Brendan Eich';
 console.log(man.fullName); // Brendan Eich
+
+Object.defineProperty({}, 'prop', {
+  get() {
+    return 1;
+  },
+  value: 1
+}); // Error: Invalid property descriptor.
+
+let user = {
+  get name() {
+    return this._name;
+  },
+  set name(value) {
+    if (value.length < 4) {
+      console.log('Name is too short, need at least 4 characters');
+      return;
+    }
+    this._name = value;
+  }
+};
+
+user.name = 'Pete';
+console.log(user._name); // Pete
