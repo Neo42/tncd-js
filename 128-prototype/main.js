0ce6aa5a3d524415112623a22e1cccc6fe131019
墨石 <582307236@qq.com>
2019-11-12 17:53:08 +0800

    🚀🚀🚀 Let's Go!!! 🚀🚀🚀

--- 128-prototype/main.js
+++ 128-prototype/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let author = { name: 'Philip K Dick' };
-let human = {
+let android = {
   dream: 'electronic sheep',
   say() {
     console.log(
@@ -11,11 +11,11 @@ let human = {
 };
 
 console.log(author.dream); // undefined
-author.__proto__ = human;
+author.__proto__ = android;
 console.log(author); // { name: 'Philip K Dick' }
 console.log(author.dream); // Electronic Sheep
 
-author = { name: 'Philip K Dick', __proto__: human };
+author = { name: 'Philip K Dick', __proto__: android };
 console.log(author.dream); // electronic sheep
 author.say(); // Reality is that which, when you stop believing in it, doesn't go away.
 
@@ -24,3 +24,7 @@ author.say = function() {
 };
 
 author.say(); // A man is an angel that has gone deranged.
+
+let creature = { die: true };
+android.__proto__ = creature;
+console.log(android.die); // true
