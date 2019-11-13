3392607445ecc3f740aa4fb71da16ecc40c63c02
墨石 <582307236@qq.com>
2019-11-13 17:50:01 +0800

    🚀🚀🚀 Let's Go!!! 🚀🚀🚀

--- 133-class-expression/main.js
+++ 133-class-expression/main.js
@@ -9,3 +9,22 @@ let Servant = class {
 		console.log(this.weapon + '!!!');
 	}
 };
+
+let saber = new Servant('Saber', 'Excalibur');
+saber.attack();
+
+let makeServant = function() {
+	return class {
+		constructor(name, weapon) {
+			this.name = name;
+			this.weapon = weapon;
+		}
+		attack() {
+			console.log(this.weapon + '!!!');
+		}
+	};
+};
+
+Servant = makeServant();
+saber = new Servant('Saber', 'Excalibur');
+saber.attack(); // Excalibur!!!
