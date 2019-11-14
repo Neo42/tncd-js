135b14ae5b63123aedd4e54c4fc604e1ad446dba
墨石 <582307236@qq.com>
2019-11-14 16:29:01 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 138-class-super-explained/main.js
+++ 138-class-super-explained/main.js
@@ -1,31 +1,57 @@
 'use strict';
-let animal = {
-	name: 'Animal',
-	eat() {
-		console.log(`${this.name} eats.`);
+let Servant = {
+	name: 'Servant',
+	weapon: undefined,
+	doSomething() {
+		console.log(`${this.name} attacks with the ${this.weapon}.`);
 	}
 };
 
-let rabbit = {
-	__proto__: animal,
-	name: 'Rabbit',
-	eat() {
+let Saber = {
+	__proto__: Servant,
+	name: 'Saber',
+	weapon: 'アホ毛',
+	doSomething() {
 		// that's how super.eat() could presumably work
-		this.__proto__.eat.call(this); // => 当 this = LongEar 时，运行结果为 Rabbit.eat.call(LongEar)，跟之前的方法一样，所以会构成无限循环的递归
+		this.__proto__.doSomething.call(this); // => 当 this = LongEar 时，运行结果为 Rabbit.eat.call(LongEar)，跟之前的方法一样，所以会构成无限循环的递归
 		console.log(this); // Rabbit
 	}
 };
 
-let longEar = {
-	__proto__: rabbit,
-	name: 'LongEar',
-	eat() {
+let KingArthur = {
+	__proto__: Saber,
+	name: 'King Arthur',
+	weapon: 'Excalibur',
+	doSomething() {
 		// ...do something with long ears and call parent (rabbit) method
 		console.log(this.__proto__); // { name: 'Rabbit', eat: [λ: eat] }
 		// this.__proto__.eat.call(this); // => this === LongEar, 所以运行结果为 Rabbit.eat.call(LongEar)，于是会往上找接下来应该运行 Rabbit.eat() 的方法
 	}
 };
 
-rabbit.eat(); // Rabbit eats.
-longEar.eat();
-// { name: 'Rabbit', eat: [λ: eat] } { name: 'Rabbit', eat: [λ: eat] }
+Saber.doSomething();
+// Saber attacks with the アホ毛.
+// { name: 'Saber', weapon: 'アホ毛', doSomething: [λ: doSomething] }
+KingArthur.doSomething(); // { name: 'Saber', weapon: 'アホ毛', doSomething: [λ: doSomething] }
+
+Saber = {
+	__proto__: Servant,
+	name: 'Saber',
+	weapon: 'アホ毛',
+	doSomething() {
+		// rabbit.eat.[[HomeObject]] == rabbit
+		super.doSomething();
+	}
+};
+
+KingArthur = {
+	__proto__: Saber,
+	name: 'King Arthur',
+	weapon: 'Excalibur',
+	doSomething() {
+		// longEar.eat.[[HomeObject]] == longEar
+		super.doSomething();
+	}
+};
+
+KingArthur.doSomething(); // King Arthur attacks with the Excalibur.
