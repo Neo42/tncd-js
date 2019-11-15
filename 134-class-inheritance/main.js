2f9daa1ff030d0f857f2070717be0fb4b000bbf2
墨石 <582307236@qq.com>
2019-11-15 17:02:45 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 134-class-inheritance/main.js
+++ 134-class-inheritance/main.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-'use strict'
-
-class Servant {
-  constructor (name, weapon) {
-    this.name = name
-    this.weapon = weapon
-  }
-
-  attack () {
-    console.log(this.weapon + '!!!')
-  }
-}
-
-class Saber extends Servant {
-  // 如果没有附加属性要添加则可以完全省略 constructor
-  constructor (master, ...args) {
-    super(...args)
-    this.master = master
-  }
-
-  say () {
-    console.log(`${this.master}! 結婚しよ!`)
-  }
-
-  attack () {
-    super.attack()
-    console.log('アホ毛!!!')
-  }
-}
-
-console.log(Saber) // [λ: Saber]
-const kingArthur = new Saber('墨石', 'Arthur', 'Excalibur')
-console.log(kingArthur) // Saber { name: 'Arthur', weapon: 'Excalibur', master: '墨石' }
-kingArthur.say() // 墨石! 結婚しよ!
-kingArthur.attack() // Excalibur!!! アホ毛!!!
-=======
 'use strict';
 
 class Servant {
@@ -67,4 +30,3 @@ let kingArthur = new Saber('墨石', 'Arthur', 'Excalibur');
 console.log(kingArthur); // Saber { name: 'Arthur', weapon: 'Excalibur', master: '墨石' }
 kingArthur.say(); // 墨石! 結婚しよ!
 kingArthur.attack(); // Excalibur!!! アホ毛!!!
->>>>>>> 0b29fcf16835ccfe0431a8d93d1c052670436f49
--- 138-class-getter-setter/main.js
+++ 138-class-getter-setter/main.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-'use strict'
-
-let thing
-class Servant {
-  constructor (name, weapon) {
-    this.name = name
-    this.weapon = weapon
-  }
-
-  get name () {
-    return this._name
-  }
-
-  set name (value) {
-    if (value.length < 4) {
-      console.error('The name is too short.')
-      return
-    }
-    this._name = value
-  }
-}
-
-const saber = new Servant('Saber', 'Excalibur')
-console.log(saber.name) // Saber
-=======
 'use strict';
 
 let thing;
@@ -47,4 +21,3 @@ class Servant {
 
 let saber = new Servant('Saber', 'Excalibur');
 console.log(saber.name); // Saber
->>>>>>> 0b29fcf16835ccfe0431a8d93d1c052670436f49
