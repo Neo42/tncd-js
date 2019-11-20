c17db6f7945963a9a5e52f6c9334db38b6bdc29f
Jiang Hao <582307236@qq.com>
2019-11-20 20:36:14 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 143-mixins/main.js
+++ 143-mixins/main.js
@@ -3,13 +3,23 @@ let servantMixin = {
     console.log(`${this.weapon}!!!`);
   },
   eat() {
-    console.log(`Guh! I'm full!`);
+    console.log(`我饱了!`);
+  }
+};
+
+let saberMixin = {
+  __proto__: servantMixin,
+  attack() {
+    super.attack();
+  },
+  eat() {
+    super.eat();
   }
 };
 
 class Saber {
   weapon = 'Excalibur';
-}
+} // Saber.prototype
 
-Object.assign(Saber.prototype, servantMixin);
+Object.assign(Saber.prototype, saberMixin);
 new Saber().attack(); // Excalibur!!!
