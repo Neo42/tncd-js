80345acaee73dbd11c13abc2556535603387c6c9
墨石 <582307236@qq.com>
2019-11-27 18:11:54 +0800

    🚀🚀🚀 You're A badass!!! 🚀🚀🚀

--- 148-asynchronous-promise/main.js
+++ 148-asynchronous-promise/main.js
@@ -1,34 +1,13 @@
-'use strict';
+let toggle = false;
+let promise = new Promise((goodFunc, badFunc) =>
+  toggle
+    ? setTimeout(() => goodFunc('complete'), 5000)
+    : badFunc(new Error('Toggle is off.'))
+);
 
-function loadScript(src) {
-  return new Promise((resolve, reject) => {
-    let script = document.createElement('script');
-    script.src = src;
-    // 当脚本加载完成之后再运行回调函数
-    script.onload = resolve;
-    script.onerror = () => reject(src);
-    document.head.append(script);
+console.log(toggle);
+promise
+  .then(success => console.log(`Mission ${success}.`))
+  .catch(failure => {
+    throw failure;
   });
-}
-
-// 回调地狱、DOOM PYRAMID 末日金字塔
-
-let scriptPromise = Promise.all([
-  loadScript('https://threejs.org/build/three.jss'),
-  loadScript('https://cdn.jsdelivr.net/npm/vue/dist/vue.js'),
-  loadScript('https://unpkg.com/react@16/umd/react.development.js')
-])
-  .then(() => {
-    console.log(Vue);
-    console.log(React);
-    console.log(THREE);
-  })
-  .catch(src => {
-    console.log(`Failed to load script from ${src}`);
-  });
-
-// 同步操作时会显示 THREE 未定义，因为 log 不会等 three.js 脚本加载完成之后再运行，而是 three.js 开始加载之后直接运行 log
-
-//
-//
-//
