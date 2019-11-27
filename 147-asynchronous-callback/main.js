aea3944bbf5488722c4268cdac816269651c2f97
Jiang Hao <582307236@qq.com>
2019-11-28 01:31:29 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 147-asynchronous-callback/main.js
+++ 147-asynchronous-callback/main.js
@@ -1,1 +1,26 @@
-'use strict';
+function showLaunch() {
+  let url = json.responseJSON.links.flickr_images[4];
+  let img = document.createElement('img');
+  img.src = url;
+  img.width = 600;
+  setTimeout(() => document.querySelector('body').append(img), 5000);
+}
+
+function showError() {
+  console.log(`Request failed.`);
+}
+
+let json = $.getJSON({
+  url: `https://api.spacexdata.com/v3/launches/67`,
+  success: showLaunch,
+  error: showError
+});
+
+let i = 6;
+let interval = setInterval(() => {
+  i -= 1;
+  console.log(i);
+  if (i === 0) {
+    clearInterval(interval);
+  }
+}, 1000);
