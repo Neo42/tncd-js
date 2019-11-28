57f5f50326df45a5f41072cd6149c25c7ce384a3
墨石 <582307236@qq.com>
2019-11-28 14:02:06 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 148-asynchronous-promise/main.js
+++ 148-asynchronous-promise/main.js
@@ -1,13 +1,43 @@
-let toggle = false;
-let promise = new Promise((goodFunc, badFunc) =>
-  toggle
-    ? setTimeout(() => goodFunc('complete'), 5000)
-    : badFunc(new Error('Toggle is off.'))
-);
+let charged = true;
+let car = {
+  control: new Promise((go, stop) => {
+    if (charged) {
+      go();
+    } else {
+      stop();
+    }
+  }),
 
-console.log(toggle);
-promise
-  .then(success => console.log(`Mission ${success}.`))
-  .catch(failure => {
-    throw failure;
-  });
+  goFoward(distance) {
+    console.log(`Car moved forward for ${distance}.`);
+  },
+
+  goLeft(distance) {
+    console.log(`Car moved left for ${distance}.`);
+  },
+
+  batteryDead() {
+    console.log(`Battery is dead.`);
+    charged = false;
+  },
+  breakDown() {
+    console.log(`Car broke down`);
+  }
+};
+
+car.control
+  .then(() =>
+    setTimeout(() => {
+      car.goFoward(`500 meters`);
+    }, 1000)
+  )
+  .then(() =>
+    setTimeout(() => {
+      car.goLeft(`500 meters`);
+    }, 2000)
+  )
+  .then(() =>
+    setTimeout(() => {
+      car.batteryDead();
+    }, 3000)
+  );
