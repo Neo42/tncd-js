e14d82bfcefc6f8e0c7d6d6bf699a33fea325a7a
墨石 <582307236@qq.com>
2019-11-29 16:35:23 +0800

    🚀🚀🚀 Let's Go!!! 🚀🚀🚀

--- 146-asynchronous-origin/main.js
+++ 146-asynchronous-origin/main.js
@@ -1,7 +1,7 @@
 // https://developer.mozilla.org/zh-CN/docs/Glossary/%E5%BC%82%E6%AD%A5
 // Computer science (In computer science, especially parallel computing, synchronization refers to the coordination of simultaneous threads or processes to complete a task with correct runtime order and no unexpected race conditions.)
-// https://en.wikipedia.org/wiki/Synchronization
-// https://en.wikipedia.org/wiki/Synchronization_(computer_science)
+// 同步：https://en.wikipedia.org/wiki/Synchronization
+// 同步（计算机科学）：https://en.wikipedia.org/wiki/Synchronization_(computer_science)
 
 // 平行计算：https://en.wikipedia.org/wiki/Parallel_computing
 // 竞争状态：https://en.wikipedia.org/wiki/Race_condition
