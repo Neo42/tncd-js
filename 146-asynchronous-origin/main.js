e6a584bba9289430cc8db6f6ab0d08e611934edc
墨石 <582307236@qq.com>
2019-11-29 15:38:59 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

---
+++ 146-asynchronous-origin/main.js
@@ -0,0 +1,7 @@
+// Computer science (In computer science, especially parallel computing, synchronization refers to the coordination of simultaneous threads or processes to complete a task with correct runtime order and no unexpected race conditions.)
+// https://en.wikipedia.org/wiki/Synchronization
+// https://en.wikipedia.org/wiki/Synchronization_(computer_science)
+
+// 平行计算：https://en.wikipedia.org/wiki/Parallel_computing
+// 竞争状态：https://en.wikipedia.org/wiki/Race_condition
+// 多线程：https://en.wikipedia.org/wiki/Thread_(computing)
