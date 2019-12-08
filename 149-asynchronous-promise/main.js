ebc08f211cae390e75c21892b85a2707fe59879f
Jiang Hao <582307236@qq.com>
2019-12-08 17:46:24 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 149-asynchronous-promise/main.js
+++ 149-asynchronous-promise/main.js
@@ -1,3 +1,5 @@
+// https://promisesaplus.com/
+
 let res = 'I am a response.';
 let promise = new Promise((onThen, onCatch) => {
   if (res) {
@@ -29,30 +31,30 @@ promise
 // 2. reject with new Error
 // 3. 可以同步执行
 
-// function showPokemon(res) {
-//   let imgUrl = res.sprites.front_default;
-//   let img = document.createElement('img');
-//   img.height = 300;
-//   img.src = imgUrl;
-//   document.body.appendChild(img);
-// }
+function showPokemon(res) {
+  let imgUrl = res.sprites.front_default;
+  let img = document.createElement('img');
+  img.height = 300;
+  img.src = imgUrl;
+  document.body.appendChild(img);
+}
 
-// function showError(e) {
-//   console.log(`Request failed: ${e}`);
-// }
+function showError(e) {
+  console.log(`Request failed: ${e}`);
+}
 
-// function getPokemon(id) {
-//   /*return new Promise((resolve, reject) => {})*/
-//   return new Promise((onThen, onCatch) => {
-//     $.getJSON({
-//       url: `https://pokeapi.co/api/v2/pokemon/${id}/`,
-//       success: onThen,
-//       error: onCatch
-//     });
-//   });
-// }
+function getPokemon(id) {
+  /*return new Promise((resolve, reject) => {})*/
+  return new Promise((onThen, onCatch) => {
+    $.getJSON({
+      url: `https://pokeapi.co/api/v2/pokemon/${id}/`,
+      success: onThen,
+      error: onCatch
+    });
+  });
+}
 
-// console.log(getPokemon(25));
-// getPokemon(25)
-//   .then(showPokemon)
-//   .catch(showError);
+console.log(getPokemon(25));
+getPokemon(25)
+  .then(showPokemon)
+  .catch(showError);
