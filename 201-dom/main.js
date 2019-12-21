1cda24b7331c72a5a17aebf2627aac9dad868e7b
Jiang Hao <582307236@qq.com>
2019-12-21 20:47:30 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 201-dom/main.js
+++ 201-dom/main.js
@@ -1,3 +1,18 @@
+console.log(document.body.childNodes); // NodeList(6) [text, div, text, ul, text, script]
+for (let i = 0; i < document.body.childNodes.length; i++) {
+  console.log(`The ${i + 1}th child node: ${document.body.childNodes[i]}`);
+}
+
+console.log(
+  `Body's parent node: ${document.body.parentNode}`,
+  document.body.parentNode.nodeName
+); // <html></html> HTML
+console.log(document.body.childNodes[0]); // <head></head>
+console.log(document.body.firstChild); // #text
+console.log(document.body.lastChild); // <script></script>
+console.log(document.body.lastChild.previousSibling); // #text
+console.log(document.body.firstChild.nextSibling); // <div></div>
+
 document.body.firstElementChild.style.color = '#6c7b95';
 console.log(document.body.firstElementChild); // true
 
