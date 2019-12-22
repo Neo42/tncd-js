fb20a2701a5cae29cfed5432818c83f1686bbc72
Jiang Hao <582307236@qq.com>
2019-12-22 16:13:31 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 201-dom/main.js
+++ 201-dom/main.js
@@ -1,25 +1,27 @@
-console.log(document.body.childNodes); // NodeList(6) [text, div, text, ul, text, script]
+console.log(document.querySelector('ul').childNodes); // NodeList(7) [text, li, text, li, text, li, text]
+
 for (let i = 0; i < document.body.childNodes.length; i++) {
   console.log(`The ${i + 1}th child node: ${document.body.childNodes[i]}`);
 }
 
+console.log(`Body's parent node: ${document.body.parentNode.nodeName}`); // <html></html> HTML
+console.log(`Body's first child node: ${document.body.childNodes[0].nodeName}`); // <html></html> HTML
+console.log(`Body's firt child node: ${document.body.firstChild.nodeName}`); // <html></html> HTML
+console.log(`Body's last child node: ${document.body.lastChild.nodeName}`); // <html></html> HTML
 console.log(
-  `Body's parent node: ${document.body.parentNode}`,
-  document.body.parentNode.nodeName
+  `Body's second last child node: ${document.body.lastChild.previousSibling.nodeName}`
 ); // <html></html> HTML
-console.log(document.body.childNodes[0]); // <head></head>
-console.log(document.body.firstChild); // #text
-console.log(document.body.lastChild); // <script></script>
-console.log(document.body.lastChild.previousSibling); // #text
-console.log(document.body.firstChild.nextSibling); // <div></div>
+console.log(
+  `Body's second first child node: ${document.body.firstChild.nextSibling.nodeName}`
+); // <html></html> HTML
 
 document.body.firstElementChild.style.color = '#6c7b95';
 console.log(document.body.firstElementChild); // true
 
-document.body.firstElementChild.nextElementSibling.style.color = '#8bbabb';
+document.body.firstElementChild.nextElementSibling.style.color = '#6c7b95';
 console.log(document.body.firstElementChild.nextElementSibling);
 document.body.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.style.color =
-  '#6c7b95';
+  '#8bbabb';
 console.log(
   document.body.firstElementChild.nextElementSibling.firstElementChild
     .nextElementSibling
