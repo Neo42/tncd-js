56af7aca02ba87df7bf3b9bfefa519aa85ea2dee
Jiang Hao <582307236@qq.com>
2019-12-24 21:54:18 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 202-selector/main.js
+++ 202-selector/main.js
@@ -1,5 +1,5 @@
-//
+// id 必须唯一
 console.log(users); // <ul id="users">...</ul>
 console.log(window['user-1']); // <li id="user-1">John</li>
 console.log(document.getElementById('user-2')); // <li id="user-2">Pete</li>
-console.log(document.querySelectorAll('ul>li:last-child')[0]); // <li id="user-3">Tom</li>
+console.log(document.querySelectorAll('ul>li:last-child')[0]); // <li id="user-3">Tom</li> querySelectorAll返回一个数组
