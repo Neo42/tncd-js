42cd4cdb0dd0f0b45db3f36163cdabe2d56d5bdf
Jiang Hao <582307236@qq.com>
2019-12-25 21:21:05 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 201-dom/main.js
+++ 201-dom/main.js
@@ -1,46 +1,21 @@
-console.log(document.querySelector('ul').childNodes); // NodeList(7) [text, li, text, li, text, li, text]
+let html = document.documentElement;
+let head = document.head;
+let body = document.body;
+console.log(html, head, body); // <html></html> <head></head> <body></body>
 
-// DOM集合：只读、实时、不可 for...in 循环
-for (let i = 0; i < document.body.childNodes.length; i++) {
-  console.log(`The ${i + 1}th child node: ${document.body.childNodes[i]}`);
+// 只会显示 script 元素之前（包括 script）的元素
+console.log(body.firstChild); // #text:"↵    " body 和 div 之间的所有字符
+console.log(body.lastChild); // script
+console.log(body.childNodes, typeof body.childNodes); // NodeList(8) [text, div, text, ul, text, table#table, text, script], "object"
+
+for (const elem of body.childNodes) {
+  console.log(elem); // 展示 script元素之前的所有元素
 }
 
-console.log(`Body's parent node: ${document.body.parentNode.nodeName}`); // <html></html> HTML
-console.log(`Body's first child node: ${document.body.childNodes[0].nodeName}`); // <html></html> HTML
-console.log(`Body's firt child node: ${document.body.firstChild.nodeName}`); // <html></html> HTML
-console.log(`Body's last child node: ${document.body.lastChild.nodeName}`); // <html></html> HTML
-console.log(
-  `Body's second last child node: ${document.body.lastChild.previousSibling.nodeName}`
-); // <html></html> HTML
-console.log(
-  `Body's second first child node: ${document.body.firstChild.nextSibling.nodeName}`
-); // <html></html> HTML
+console.log(body.children); // 展示 script 标签前的所有元素节点
+console.log(body.firstElementChild); // div
+console.log(body.firstElementChild.nextElementSibling); // ul
+console.log(body.lastElementChild.parentElement); // body
 
-document.body.firstElementChild.style.color = '#6c7b95';
-console.log(document.body.firstElementChild); // true
-
-document.body.firstElementChild.nextElementSibling.style.color = '#6c7b95';
-console.log(document.body.firstElementChild.nextElementSibling);
-document.body.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.style.color =
-  '#8bbabb';
-console.log(
-  document.body.firstElementChild.nextElementSibling.firstElementChild
-    .nextElementSibling
-);
-
-console.log(table.rows); // HTMLCollection(2) [tr, tr] 所有行元素的集合
-let cap = table.createCaption(); // 添加说明
-cap.innerHTML = '<b>YOLO</b>'; // 添加说明文字
-console.log(table.caption); // <caption><b>YOLO</b></caption>
-table.rows[0].cells[1].style.color = '#faf9f9';
-console.log(table.tBodies); // // HTMLCollection[tbody]
-table.tBodies[0].style.backgroundColor = '#add2c9';
-console.log(table.rows[0].sectionRowIndex); // 0 行元素索引数
-console.log(table.rows[1].sectionRowIndex); // 1
-let thead = table.createTHead();
-thead.innerText = 'Head';
-let tfoot = table.createTFoot();
-tfoot.innerText = 'Foot';
-console.log(table.tHead, table.tFoot); // <thead>Head</thead> <tfoot>Foot</tfoot>
-
-// table.tBodies.style.backgroundColor = '#d6e5fa';
+console.log(document.documentElement.parentNode); // #document
+console.log(document.documentElement.parentElement); // null, document 不是元素节点
--- 203-selectors/main.js
+++ 203-selectors/main.js
@@ -3,3 +3,4 @@ console.log(users); // <ul id="users">...</ul>
 console.log(window['user-1']); // <li id="user-1">John</li>
 console.log(document.getElementById('user-2')); // <li id="user-2">Pete</li>
 console.log(document.querySelectorAll('ul>li:last-child')[0]); // <li id="user-3">Tom</li> querySelectorAll返回一个数组
+console.log(users.contains(document.getElementById('user-1'))); // true
