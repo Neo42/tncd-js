b9978f4a166a142ff661c2f4bd5a014c577d248b
Jiang Hao <582307236@qq.com>
2020-01-10 22:48:18 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 204-node-properties/main.js
+++ 204-node-properties/main.js
@@ -4,18 +4,29 @@ let p = document.body.firstChild;
 console.log(p.nodeName, p.tagName, p.nodeType); // #text undefined 3
 
 console.log(ele.innerHTML, `\n`, ele.innerText);
-//  Blah
-//  <script src="./main.js"></script>
-//  Blah
+// Blah
+// < !--comment-- >
+// <div id="txt"></div>
+// <script src="./main.js"></script>
+// Blah
 
 // ele.innerHTML = 'New body';
 // => e.innerHTML = 'New body';
 // console.log(ele.innerHTML); // New body
 
-ele.outerHTML = 'Bruh'; // 页面可以看到新文本节点
-console.log(ele.outerHTML); // 仍然是旧元素节点，因为新节点并没有被存储到任何变量里
+// ele.outerHTML = 'Bruh'; // 页面可以看到新文本节点
+// console.log(ele.outerHTML); // 仍然是旧元素节点，因为新节点并没有被存储到任何变量里
 // <body>
-//  Blah
-//  <script src="./main.js"></script></body>
+// Blah
+// <!-- comment -->
+// <div id="txt"></div>;
+// <script src="./main.js"></script></body>
 
 console.log(p.nodeValue, p.data); // Blah Blah
+console.log(p.nextSibling.data); // comment
+
+let name = prompt("What's your name?", "<b>I should't be bold.</b>");
+txt.innerHTML = name; // 插入 html
+txt.textContent = name; // 插入纯文本
+
+setTimeout(() => (hidden.hidden = !hidden.hidden), 5000); //  5秒后切换 #hidden 的隐藏属性
