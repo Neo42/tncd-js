14a8f632f11e16da805aebcc3f047a40da4b0207
Jiang Hao <582307236@qq.com>
2020-02-19 15:45:31 +0800

    🚀🚀🚀 Another awesome commit!!! 🚀🚀🚀

--- 209-element-size/main.js
+++ 209-element-size/main.js
@@ -1,10 +1,19 @@
-console.log(document.querySelector('#example').offsetParent.id); // main
-// offsetWidth = borderWidth + padding + contentWidth + padding + scrollbarWidth + borderWidth
-console.log(document.querySelector('#example').offsetWidth); // 390 = 300 (284 + 16) + 20 * 2 + 25 * 2
+let example = document.querySelector('#example');
+console.log(example.offsetParent.id); // main
+// offsetWidth = borderWidth + padding + contentWidth + padding + scrollbarWidth(Windows) + borderWidth
+console.log(example.offsetWidth); // 390 = 300 (284 + 16) + 20 * 2 + 25 * 2
 // offsetHeight = borderWidth + padding + contentHeight + padding + borderWidth
-console.log(document.querySelector('#example').offsetHeight); // 290 = 200 + 20 * 2 + 25 * 2
-console.log(document.querySelector('#example').offsetLeft); // 200
-console.log(document.querySelector('#example').offsetTop); // 200
+console.log(example.offsetHeight); // 290 = 200 + 20 * 2 + 25 * 2
+console.log(example.offsetLeft); // 200
+console.log(example.offsetTop); // 200
 
 // check if the current element is hidden
 const isHidden = elem => !elem.offsetHeight && !elem.offsetHeight;
+console.log(isHidden(example));
+
+// clientHeight = contentHeight + padding * 2
+console.log(example.clientHeight); // 240 = 200 + 20 * 2
+// clientHeight = contentWidth + padding * 2
+console.log(example.clientWidth); // 340 = 300 + 20 * 2
+console.log(example.scrollHeight); // 590
+example.style.height = `${example.scrollHeight}px`;
